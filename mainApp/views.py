d05e2acba957b27184966454f9e2bea851d9d8f5
a20210115 <a20210115@utem.edu.mx>
2024-11-01 15:01:34 -0600

    cambios

---
+++ mainApp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+# Create your views here.
+def inicio(request):
+    return render(request, 'index.html')
+
+def dashboard(request):
+    return render(request, 'sidebar.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def formAgg(request):
+    return render(request, 'formAgregarLibro.html')
+
+def prestamos(request):
+    return render(request, 'Prestamos.html')
+
+def formPL(request):
+    return render(request, 'formPrestamo.html')
+
+def Tesis(request):
+    return render(request, 'Tesis.html')
+
+def formTesis(request):
+    return render(request, 'formTesis.html')
